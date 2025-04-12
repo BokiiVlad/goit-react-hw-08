d782fb438f24ec9d90e231eed5814d5ed6c96e79
Vlad Bokii <bokiyvladik10@gmail.com>
2025-04-12 21:03:00 +0300

    hw

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,13 +2,14 @@ import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { IoHomeSharp } from "react-icons/io5";
 
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <div>
       <NavLink className={css.link} to="/">
-        Home
+        <IoHomeSharp className={css.icon} />
       </NavLink>
       {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </div>
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import fetchContacts from "../../redux/contacts/operations";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectContacts } from "../../redux/contacts/selectors";
 
 const ContactsPage = () => {
+  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
+  return contacts.map();
 };
+
 export default ContactsPage;
